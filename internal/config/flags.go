c77f1f47d9d532d0f56b7f8131dbce0d2bb3bb64
kukushkin <denlinkers@gmail.com>
2023-12-20 15:06:34 +0300

    debug cookie domain

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -2,10 +2,13 @@ package config
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 func ParseFlags() *ServiceConfig {
 	var conf ServiceConfig
+	fmt.Printf("Service started with flags: %v\n", os.Args)
 	flag.StringVar(&conf.Addr, "a", defaultAddress, "address to run server")
 	flag.StringVar(&conf.ShortURLHost, "b", defaultShortURL, "base address to result short URL")
 	flag.StringVar(&conf.LoggerLevel, "l", defaultLevel, "logger level")
