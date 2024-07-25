ddda98004c72a49e339b810e5482a1f6cc5c5b4c
kukushkin <denlinkers@gmail.com>
2024-07-25 17:46:20 +0300

    iter18 add examples

--- example_test.go
+++ example_test.go
@@ -1,11 +1,38 @@
 package urlshort
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
+	"fmt"
+	"github.com/GearFramework/urlshort/internal/app"
+	"github.com/GearFramework/urlshort/internal/pkg/auth"
 )
 
-func TestExample(t *testing.T) {
-	a := true
-	assert.Equal(t, true, a)
+// ExampleGetID example of generated unique user ID
+func ExampleGetID() {
+	gen := app.UserGenID{}
+	userID := gen.GetID()
+	fmt.Println(userID)
+}
+
+// ExampleBuildJWT example create token by user ID
+func ExampleBuildJWT() {
+	gen := app.UserGenID{}
+	userID := gen.GetID()
+	token, err := auth.BuildJWT(userID)
+	if err != nil {
+		fmt.Printf("jwt error: %s\n", err)
+	}
+	fmt.Println(token)
+}
+
+// ExampleGetUserIDFromJWT example get user from token
+func ExampleGetUserIDFromJWT() {
+	gen := app.UserGenID{}
+	userID := gen.GetID()
+	token, err := auth.BuildJWT(userID)
+	if err != nil {
+		fmt.Printf("jwt error: %s\n", err)
+	}
+	fmt.Println(token)
+	userID = auth.GetUserIDFromJWT(token)
+	fmt.Println(userID)
 }
